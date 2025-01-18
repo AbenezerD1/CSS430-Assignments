aff1ec0b6b5acf3e272f82ac5b8ceb46c674dd4b
AbenezerD1 <150618491+AbenezerD1@users.noreply.github.com>
2025-01-17 18:15:24 -0800

    working ps command

--- code/Program1/Cpp/processes.cpp
+++ code/Program1/Cpp/processes.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <iostream> // for cout
 #include <stdio.h>     // for NULL, perror
 #include <stdlib.h>    // for exit
@@ -8,6 +9,7 @@
 using namespace std;
 
 int main(int argc, char **argv) {
+  enum {RD,WR};
   int fds[2][2];
   int pid;
 
@@ -23,19 +25,45 @@ int main(int argc, char **argv) {
     // I'm a child
 
     // create a pipe using fds[0]
-    // fork a grand-child
-    // if I'm a grand-child
-    // create a pipe using fds[1]
-    // fork a great-grand-child
-    // if I'm a great-grand-child
-    // execute "ps"
-    // else if I'm a grand-child
-    // execute "grep"
+    if(pipe(fds[0]) < 0){
+      perror("pipe error");
+    }else if((pid = fork()) < 0){ // fork a grand-child
+      perror("fork 2 error");
+    }else if(pid == 0){
+      // if I'm a grand-child
+
+      if(pipe(fds[1]) < 0){ // create a pipe using fds[1]
+        perror("pipe 2 error");
+      }else if((pid = fork()) < 0){ // fork a great-grand-child
+        perror("fork 3 error");
+      }else if(pid == 0){
+        // if I'm a great-grand-child
+        // execute "ps"
+        //close(fds[1][RD]);
+        //dup2(fds[1][WR], 0);
+        
+        execlp("/bin/ps","ps","-A", NULL);
+      }else{
+        wait(NULL);
+      }
+      // else if I'm a grand-child
+      // execute "grep"
+      //close(fds[1][RD]);
+      //dup2(fds[1][WR],0); // stdin --> great grand child stdout
+      
+      //execlp("/bin/grep", "grep",argv[1],NULL);
+    }else{
+      
+      wait(NULL);
+    }
     // else if I'm a child
     // execute "wc"
+
+    // execlp("/bin/wc", "wc","-l",NULL);
   } else {
     // I'm a parent
     wait(NULL);
     cout << "commands completed" << endl;
   }
+  exit(EXIT_SUCCESS);
 }
