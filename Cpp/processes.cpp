878976027d253a9b1bb28264fa794d107a239fcb
AbenezerD1 <150618491+AbenezerD1@users.noreply.github.com>
2025-01-22 17:19:34 +0000

    fixed processes file

--- Cpp/processes.cpp
+++ Cpp/processes.cpp
@@ -18,49 +18,48 @@ int main(int argc, char **argv) {
     exit(-1);
   }
 
-  // fork a child
-  if ((pid = fork()) < 0) {
-    perror("fork error");
-  } else if (pid == 0) {
-    // I'm a child
+  
+  if ((pid = fork()) < 0) { // fork a child process
+    perror("Error forking a child process");
+  } else if (pid == 0) { 
+    // I'm a child process
 
-    // create a pipe using fds[0]
     if(pipe(fds[0]) < 0){
-      perror("pipe error");
+      perror("Error creating a pipe using fds[0]");
     }else if((pid = fork()) < 0){ // fork a grand-child
-      perror("fork 2 error");
+      perror("Error forking a grand child process from child process");
     }else if(pid == 0){
-      // if I'm a grand-child
+      //I'm a grand-child process
 
       if(pipe(fds[1]) < 0){ // create a pipe using fds[1]
-        perror("pipe 2 error");
+        perror("Error creating a pipe using fds[1]");
       }else if((pid = fork()) < 0){ // fork a great-grand-child
-        perror("fork 3 error");
+        perror("Error forking a great grand child process from a grand child process");
       }else if(pid == 0){
-        // if I'm a great-grand-child
-        // execute "ps"
+        //I'm a great-grand-child
 
         close(fds[1][RD]);
-        dup2(fds[1][WR], 1); // stdout --> fds[1][1]
+        dup2(fds[1][WR], 1); // stdout --> great grand child stdout
         
         execlp("/bin/ps","ps","-A", NULL);
       }else{
         wait(NULL);
       }
-      // else if I'm a grand-child
-      // execute "grep"
+      
       close(fds[1][WR]);
       dup2(fds[1][RD],0); // stdin --> great grand child stdout
       
+      close(fds[0][RD]);
+      dup2(fds[0][WR], 1); // stdout --> grand child stdout
       execlp("/bin/grep", "grep",argv[1],NULL);
     }else{
-      
       wait(NULL);
     }
-    // else if I'm a child
-    // execute "wc"
 
-    // execlp("/bin/wc", "wc","-l",NULL);
+    close(fds[0][WR]);
+    dup2(fds[0][RD], 0); // stdin --> grand child stdout
+
+    execlp("/bin/wc", "wc","-l",NULL);
   } else {
     // I'm a parent
     wait(NULL);
