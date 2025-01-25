e3cfdb7c3d0d51f631fbacf274c91e87d607644b
AbenezerD1 <150618491+AbenezerD1@users.noreply.github.com>
2025-01-25 04:49:41 +0000

    Finsihed shell

--- P1/src/Shell.java
+++ P1/src/Shell.java
@@ -1,8 +1,6 @@
-import java.io.*;
 import java.util.*;
 
-class Shell extends Thread
-{
+class Shell extends Thread {
    //command line string to contain the full command
    private String cmdLine;
    private int counter;
@@ -17,49 +15,125 @@ class Shell extends Thread
 
    // required run method for this Shell Thread
    public void run( ) {
-
-      // build a simple command that invokes PingPong 
-      //cmdLine = "PingPong abc 100";
-   
       while(!cmdLine.equalsIgnoreCase("exit")){
-         cmdLine = ProcessLine();
+         cmdLine = GetLine();
 
          if(cmdLine.isEmpty()){
             continue;
          }
 
-         if(cmdLine.contains("&")){
-            
-         }else if(cmdLine.contains(";")){
-
-         }
-
-         String[] args = SysLib.stringToArgs(cmdLine);
-         counter++;
-         
-         if(!(SysLib.exec(args) < 0)){
-            SysLib.join();
-         }else{
-            continue;
-         }
+         execute(cmdLine);
       }
-      // must have an array of arguments to pass to exec()
-      //String[] args = SysLib.stringToArgs(cmdLine);
-      //SysLib.cout("Testing PingPong\n");
-
-      // run the command
-      //int tid = SysLib.exec( args );
-      //SysLib.cout("Started Thread tid=" + tid + "\n");
-
-      // wait for completion then exit back to ThreadOS
+      
       SysLib.cout("Done!\n");
       SysLib.exit();
    }
 
-   public String ProcessLine(){
+   /**
+    * given a string of commands splits them to concurrent and sequential commands and runs them according to order they were in
+    * @param line
+    */
+    public void execute(String line){
+      boolean seqCommand = line.contains(";");
+      boolean concurrCommand = line.contains("&");
+
+      if(!seqCommand && !concurrCommand){ // single command
+         SequentialExec(line);
+         counter++;
+         return;
+      } 
+      if(seqCommand && ! concurrCommand){ // concurrent commands only
+         SequentialExec(line);
+         counter++;
+         return;
+      }
+      if(!seqCommand && concurrCommand ){ // seqential commands only
+         ConcurrExec(line);
+         counter++;
+         return;
+      }
+      // combination of concurrent and sequential commands
+
+      // always split substring using ;
+
+      // Test Cases
+      // x & y ; z
+      // x ; y & z
+      // w & x ; y & z
+      // w & x & y ; z
+      // w ; x & y ; z
+      // w ; x ; y & z
+      
+      String[] splitCmd = splitString(line, ";");
+      
+      for(String str: splitCmd){
+         ConcurrExec(str);
+      }
+      counter++;
+   }
+
+   /**
+    * print shell intro and request user to input command
+    * @return user input as a string
+    */
+   public String GetLine(){
       input = new StringBuffer();
+
       SysLib.cout("shell["+counter+"]%: ");
       SysLib.cin(input);
+
       return input.toString();
+   }
+
+   /**
+    * split string using delim, doesn't care about blank spaces
+    * @param line
+    * @param delim
+    * @return array of strings seprated using the delim
+    */
+   public String[] splitString(String line, String delim){
+      StringTokenizer tokens = new StringTokenizer(line,delim);
+      String[] toRet = new String[tokens.countTokens()];
+
+      for(int i = 0; i < toRet.length; i++){
+         toRet[i] = tokens.nextToken();
+      }
+      return toRet;
+   }
+
+   /**
+    * run commands concurrently 
+    * @param line
+    */
+   public void ConcurrExec(String line){
+      int failedProcessCounter = 0;
+      String[] commands = splitString(line, "&");
+
+      for(String cmd: commands){
+         String[] args = SysLib.stringToArgs(cmd);
+         
+         if(SysLib.exec(args) < 0){
+            failedProcessCounter++;
+         }
+      }
+
+      for(int i = 0; i < (commands.length - failedProcessCounter); i++){
+         SysLib.join();
+      }
+   }
+
+   /**
+    * runs commands sequentially
+    * @param line
+    */
+   public void SequentialExec(String line){
+      String[] commands = splitString(line, ";");
+
+      for(String cmd: commands){
+         String[] args = SysLib.stringToArgs(cmd);
+         if(!(SysLib.exec(args) < 0)){
+            SysLib.join();
+         }
+      }
    }
 }
